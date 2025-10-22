a243baf5de0184ee720f55dc88e7d67c87492915
mmd <mhdtnt1385@gmail.com>
2025-10-23 03:06:21 +0330

    added model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Food(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='images/')
+    def __str__(self):
+        return self.name
+    
+class Footer(models.Model):
+    email = models.EmailField()
+    address = models.TextField()
+    def __str__(self):
+        return self.email
